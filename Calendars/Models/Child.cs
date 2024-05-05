e8d732c416397f1d8b39934eab4288accdaaae7a
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2024-05-05 22:32:48 +0200

    - created project - added Child class

---
+++ Calendars/Models/Child.cs
@@ -0,0 +1,12 @@
+﻿namespace Calendars.Models;
+
+public class Child(string name, Date birthDate)
+{
+    private readonly string _name = name;
+    private readonly Date _birthDate = birthDate;
+
+    public override string ToString()
+    {
+        return $"{_name} was born on {_birthDate}";
+    }
+}
