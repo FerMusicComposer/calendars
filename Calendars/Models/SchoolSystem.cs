009e0a4072a8af76eb0fe720acbccdc4cbc2fe6b
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2024-05-05 22:34:08 +0200

    - added SchoolSystem class

---
+++ Calendars/Models/SchoolSystem.cs
@@ -0,0 +1,9 @@
+﻿namespace Calendars.Models;
+
+public class SchoolSystem(MonthDay cuttoffDate, int minAge, MonthDay schoolStartDate)
+{
+    private readonly MonthDay _cuttoffDate = cuttoffDate;
+    private readonly int _minAge = minAge;
+    private readonly MonthDay _schoolStartDate = schoolStartDate;
+    
+}
