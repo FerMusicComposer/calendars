9d3702235ce2b141cfc1c917dc85da70790aa9c9
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2024-05-25 13:06:33 +0200

    - refactored Program.cs

--- Calendars/Program.cs
+++ Calendars/Program.cs
@@ -2,26 +2,41 @@
 
 using Calendars.Models;
 
+// Liskov Substitution Principle
+List<Calendar> calendars = [new JulianCalendar(), new GregorianCalendar()];
+
+foreach (Calendar calendar in calendars)
+{
+    Demonstrate(calendar);
+}
+return;
+
+static void Report(List<Child> children, SchoolSystem school)
+{
+    foreach (Child child in children)
+    {
+        Console.WriteLine($"{child} starts school on {school.GetBeginning(child)}. Celebrates 1st birthday at school on {child.GetFirstCelebrationAt(school)}");
+    }    
+}
+
 static void Demonstrate(Calendar calendar)
 {
     SchoolSystem schoolSystem = new(calendar.Create(3, 1), 5, calendar.Create(8, 5));
-    Child child = new("Jane", calendar.Create(1892, 2, 29));
-    Child child2 = new("Jake", calendar.Create(1891, 8, 27));
-
+    List<Child> children =
+    [
+        new Child("John", calendar.Create(1891, 1, 1)),
+        new Child("Jane", calendar.Create(1892, 2, 29)),
+        new Child("Jake", calendar.Create(1891, 8, 27)),
+        new Child("Jill", calendar.Create(1892, 3, 7)),
+        new Child("Josh", calendar.Create(1892, 5, 1)),
+        new Child("Jess", calendar.Create(1891, 11, 3)),
+        new Child("Jose", calendar.Create(1890, 7, 13)),
+        new Child("Jessica", calendar.Create(1890, 12, 25))
+    ];
+    
     Console.WriteLine($"Using {calendar.GetName()} calendar:");
-    Report(child, schoolSystem);
-    Report(child2, schoolSystem);
+    Report(children, schoolSystem);
     Console.WriteLine();
 }
 
-static void Report(Child child, SchoolSystem school)
-{
-    Console.WriteLine($"{child} starts school on {school.GetBeginning(child)}. Celebrates 1st birthday at school on {child.GetFirstCelebrationAt(school)}");    
-}
-
-// Liskov Substitution Principle
-Calendar julian = new JulianCalendar(); 
-Calendar gregorian = new GregorianCalendar();
-
-Demonstrate(julian);
-Demonstrate(gregorian);
+  
